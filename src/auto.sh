c72744ca6df8b527fa664dac7b19116807d7eed4
Luca Diliello <lucadiliello@Lucas-MacBook-Pro.local>
2018-04-11 09:19:51 +0200

    Ready improvements2

--- src/auto.sh
+++ src/auto.sh
@@ -1,9 +1,15 @@
 # $1 n_gram_len
 # $2 n_gram_method
 
-./train.sh data/NLSPARQL.train.data out.fst $1 $2
+begin=$(date +%s)
+
+./train.sh data/NLSPARQL.train.data data/NLSPARQL.train.feats.txt out.fst $1 $2
 ./test.sh data/NLSPARQL.test.data out.fst lex.syms temp.txt
-echo "N-gram: $1 -- N-method: $2" > tmp.txt
+echo "$2 $1" > risultato.txt
 cat temp.txt | ./conlleval.pl -d "\t" | grep "accuracy:" >> risultato.txt
 
-rm out.fst lex.syms risultato.txt
+rm out.fst lex.syms temp.txt
+
+end=$(date +%s)
+echo "Total time: $(expr $end - $begin) seconds"
+cat risultato.txt
--- src/train.sh
+++ src/train.sh
@@ -1,50 +1,62 @@
 #!/bin/bash
 
 # $1 training file.data
-# $2 output fst
+# $2 training file.feats
+# $3 output fst
 
 # this script will create an fst called final.fst (and the lex.syms file)
 
-n_gram_len=2
+n_gram_len=3
 n_gram_method=witten_bell
 
-if [ ! -z $3 ]; then
-	n_gram_len=$3
+if [ ! -z $4 ]; then
+	n_gram_len=$4
 fi
 
-if [ ! -z $4 ]; then
-	n_gram_method=$4
+if [ ! -z $5 ]; then
+	n_gram_method=$5
 fi
 
 ##### Create basic input file - token,tokenstd,pos-tag,concept-tag
 
-# Create general input file
+# token,tokenstd,pos-tag,concept-tag
+./prepare_file.py $1 $2 > input.txt
 
-./O_parser.py $1 > res.txt
+cat input.txt | cut -f 3,4 > last.txt
+# pos-tag, concept-tag
+cat input.txt | cut -f 1 > first.txt
+# token
+
+# Create O-mix input file
+./O_parser.py last.txt | cut -f 2 > second.txt
+# concept-tag-mixed
+
+paste -d"\t" first.txt second.txt > final.txt
+# token, concept-tag-mixed
+
+rm input.txt last.txt first.txt second.txt
 
 # Create symbols table
-./createsymboltable.sh res.txt lex.syms
+./createsymboltable.sh final.txt lex.syms
 
 ##### token - concept fst mix (O substituted with original words)
 ## P(w|m)
-./find_probabilities.py res.txt > automa.txt
+./find_probabilities.py final.txt > automa.txt
 
 ./createfstfromtable.sh automa.txt lex.syms tmp.fst
 fstarcsort --sort_type=olabel tmp.fst > automa.fst
+rm automa.txt tmp.fst
 
 ## P(m|m-1)
-cat res.txt | cut -f 2 | sed "s/^ *$/#/g" | tr '\n' ' ' |\
+cat final.txt | cut -f 2 | sed "s/^ *$/#/g" | tr '\n' ' ' |\
 	tr '#' '\n' | sed "s/^ *//g;s/* $//g" > tmp_parsed.data
 farcompilestrings --symbols=lex.syms --unknown_symbol='<unk>' \
 	tmp_parsed.data > data.far
 ngramcount --order=$n_gram_len --require_symbols=false data.far > pos.cnt
 ngrammake --method=$n_gram_method pos.cnt | fstrmepsilon |\
 	fstdeterminize | fstminimize > concepts.fst
+rm tmp_parsed.data pos.cnt data.far final.txt
 
 ## Combining P(w|m)*P(m|m-1)
-fstcompose automa.fst concepts.fst | fstrmepsilon > $2
-
-# Cleaning
-rm res.txt automa.txt tmp.fst
-rm tmp_parsed.data pos.cnt data.far
+fstcompose automa.fst concepts.fst | fstrmepsilon > $3
 rm concepts.fst automa.fst
