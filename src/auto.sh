2b791616eba6ddd5b33e6cb2d8053801c3b97852
Luca Diliello <lucadiliello@Lucas-MacBook-Pro.local>
2018-04-15 11:14:24 +0200

    ok

--- src/auto.sh
+++ src/auto.sh
@@ -1,10 +1,15 @@
 # $1 n_gram_len
 # $2 n_gram_method
 
+begin=$(date +%s)
+
 ./train.sh data/NLSPARQL.train.data out.fst $1 $2
 ./test.sh data/NLSPARQL.test.data out.fst lex.syms temp.txt
-echo "N-gram: $1 -- N-method: $2" > risultato.txt
+echo "$2 $1" > risultato.txt
 cat temp.txt | ./conlleval.pl -d "\t" | grep "accuracy:" >> risultato.txt
 
-rm out.fst lex.syms
+rm out.fst lex.syms temp.txt
+
+end=$(date +%s)
+echo "Total time: $(expr $end - $begin) seconds" >> risultato.txt
 cat risultato.txt
